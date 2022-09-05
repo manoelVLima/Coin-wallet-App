42291431acd6bd0b9a7533b8e764334b210fbf51
Manoel Vieira Lima Junior <manoel.lima03@gmail.com>
2022-09-05 09:02:15 -0300

    Finalizei o layout do formulário de despesas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,21 @@
 import { Typography } from '@mui/material'
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { StyledToolBar } from '../styles/Header'
 function Header() {
+
+  const {user, wallet} = useSelector((state) => state);
+  console.log(user);
   return (
         <StyledToolBar>
           <Typography p={1} sx={{display: { xs: 'none', sm:'block'}}} variant="h5" fontWeight='600' fontSize='2rem'>
             COIN WALLET
           </Typography>
           <Typography variant="h6" fontWeight='600' fontSize='1.5rem'>
-            User: teste@teste.com
+            User: <em>{user.email}</em>
           </Typography>
           <Typography variant="h6" fontWeight='600' fontSize='1.5rem'>
-            R$569535
+            R$0.00
           </Typography>
 
         </StyledToolBar>
